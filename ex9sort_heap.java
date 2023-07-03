39f73bc1d6260e80483ea815b7a443da7b575f00
Petr-Poliakov <94320840+Petr-Poliakov@users.noreply.github.com>
2023-07-03 21:31:33 +0300

    Add files via upload

---
+++ ex9sort_heap.java
@@ -0,0 +1,52 @@
+public class HeapSort {
+
+    public static void heapify(int[] array, int length, int i) {
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        int largest = i;
+        if (left < length && array[left] > array[largest]) {
+            largest = left;
+        }
+        if (right < length && array[right] > array[largest]) {
+            largest = right;
+        }
+        if (largest != i) {
+            int tmp = array[i];
+            array[i] = array[largest];
+            array[largest] = tmp;
+            heapify(array, length, largest);
+        }
+    }
+
+    public static void heapSort(int[] array) {
+        if (array.length == 0) {
+            return;
+        }
+
+        int length = array.length;
+
+        for (int i = length / 2 - 1; i >= 0; i--) {
+            heapify(array, length, i);
+        }
+
+        for (int i = length - 1; i >= 0; i--) {
+            int tmp = array[0];
+            array[0] = array[i];
+            array[i] = tmp;
+            heapify(array, i, 0);
+        }
+    }
+
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] array = { 25, 12, 6, 41, 18, 31, 53 };
+        heapSort(array);
+        printArray(array);
+    }
+}
